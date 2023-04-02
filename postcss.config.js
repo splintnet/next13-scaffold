a5d399ef080e05a1dfb63ea48c4fc110583e4041
Jonas Imping <j.imping@splintnet.de>
2023-04-02 16:12:12 +0200

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": "postcss-nested",
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nested',
     tailwindcss: {},
     autoprefixer: {},
-    "postcss-preset-env": {
-      features: { "nesting-rules": false },
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
     },
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
